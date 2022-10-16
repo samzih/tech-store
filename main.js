251b17265df1d96ae8e53bed5a05e3f452fa8d6a
Sam Azimi <samzih1337@gmail.com>
2022-10-16 18:22:44 +0200

    Created and added product data to frontpage

--- main.js
+++ main.js
@@ -20,6 +20,53 @@ function initSite() {
 
 /* Uses the loaded products data to create a visible product list on the website */
 function addProductsToWebpage() {
-    // Check your console to see that the products are stored in the listOfProducts varible.
-    console.log(listOfProducts);
+    for (const product of listOfProducts) {
+
+        // PRODUCT ELEMENTS
+        const productContainer = document.createElement("div");
+        const productTitle = document.createElement("h1");
+        const productDescription = document.createElement("p");
+        const productImage = document.createElement("img");
+        const productPrice = document.createElement("h2");
+        const productAddToCartBtn = document.createElement("button");
+        const productAddToCartIcon = document.createElement("i");
+        const productAddToCartText = document.createElement("h2");
+
+
+
+
+        // PRODUCT CONTAINER
+        productContainer.classList = ("productContainer");
+
+        // PRODUCT NAME & DESCRIPTION
+        productTitle.innerText = product.title;
+        productDescription.innerText = product.description;
+
+        // PRODUCT IMAGE
+        productImage.classList = ("productImage");
+        const productImageURL = "./assets/" + product.image;
+        productImage.setAttribute("src", productImageURL);
+        productImage.setAttribute("alt", `Picture of a ${product.title}`);
+
+        // PRODUCT PRICE
+        productPrice.innerText = `${product.price} kr`;
+
+        // ADD TO CART BUTTON, ICON & TEXT
+        productAddToCartBtn.classList = ("productBtn");
+        productAddToCartIcon.classList = ("fa-solid fa-cart-arrow-down");
+        productAddToCartText.innerText = "Lägg till i kundvagnen";
+
+
+
+
+        // PRODUCT APPENDED INSIDE CONTAINER
+        document.querySelector("main").appendChild(productContainer);
+        productContainer.appendChild(productTitle);
+        productContainer.appendChild(productDescription);
+        productContainer.appendChild(productImage);
+        productContainer.appendChild(productPrice);
+        productContainer.appendChild(productAddToCartBtn);
+        productAddToCartBtn.appendChild(productAddToCartIcon);
+        productAddToCartBtn.appendChild(productAddToCartText);
+    }
 }
